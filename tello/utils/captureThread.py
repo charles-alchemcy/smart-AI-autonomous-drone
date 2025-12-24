9267cc5ee845d9428874c39183e0ad412f9a5b36
venus1004 <CharlesDevAlchemy@gmail.com>
2025-12-23 23:11:18 -0500

    change line

--- tello/utils/captureThread.py
+++ tello/utils/captureThread.py
@@ -9,6 +9,7 @@ class Capture:
         self.frameQue = queue.Queue()
         self.framCapFlag = 1
 
+
     def startCaptureThread(self):
         self.camThread = threading.Thread(target=self._reader)
         self.camThread.daemon = True
